94ef5a229b2ff7bf6dbf7f2dd40b509b480086ed
Dmitry Kinash <dv.kinash@gmail.com>
2024-11-19 21:42:28 +0200

    read data from smart-contract

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import './App.css';
-import { TonConnectButton } from '@tonconnect/ui-react';
+import "./App.css";
+import { TonConnectButton } from "@tonconnect/ui-react";
+import { useMainContract } from "./hooks/useMainContract";
 
 function App() {
+  const {
+    contract_address,
+    counter_value,
+    recent_sender,
+    owner_address,
+    contract_balance,
+  } = useMainContract();
   return (
     <div>
-      <TonConnectButton />
+      <div>
+        <TonConnectButton />
+      </div>
+      <div>
+        <div className='Card'>
+          <b>Our contract Address: </b>
+          <span className='Hint'>{contract_address?.slice(0, 30) + "..."}</span>
+        </div>
+        <div className='Card'>
+          <b>Our contract Balance: </b>
+          <span className='Hint'>{contract_balance}</span>
+        </div>
+        <div className='Card'>
+          <b>Counter Value: </b>
+          <span className='Hint'>{counter_value ?? "Loading..."}</span>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
