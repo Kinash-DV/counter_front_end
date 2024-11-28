0e00346f83e3068207e9469c84f04e13bc7295a6
Dmitry Kinash <dv.kinash@gmail.com>
2024-11-28 23:49:25 +0200

    new operations - deposit and withdrawal

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
 import { useMainContract } from "./hooks/useMainContract";
+import { useTonConnect } from "./hooks/useTonConnect";
 
 function App() {
   const {
@@ -9,7 +10,11 @@ function App() {
     recent_sender,
     owner_address,
     contract_balance,
+    sendIncrement,
+    sendDeposit,
+    sendWithdrawalRequest
   } = useMainContract();
+  const { connected } = useTonConnect();
   return (
     <div>
       <div>
@@ -20,14 +25,44 @@ function App() {
           <b>Our contract Address: </b>
           <span className='Hint'>{contract_address?.slice(0, 30) + "..."}</span>
         </div>
-        <div className='Card'>
+        <div className='Hint'>
           <b>Our contract Balance: </b>
-          <span className='Hint'>{contract_balance}</span>
+          <span className='Hint'>{contract_balance ?? "Loading..."}</span>
         </div>
-        <div className='Card'>
+        <div className='Hint'>
           <b>Counter Value: </b>
           <span className='Hint'>{counter_value ?? "Loading..."}</span>
         </div>
+        <br />
+        {connected && (
+          <a
+            onClick={() => {
+              sendIncrement();
+            }}
+          >
+            Increment by 7
+          </a>
+        )}
+        <br />
+        {connected && (
+          <a
+            onClick={() => {
+              sendDeposit();
+            }}
+          >
+            Request deposit 1 TON
+          </a>
+        )}
+        <br />
+        {connected && (
+          <a
+            onClick={() => {
+              sendWithdrawalRequest();
+            }}
+          >
+            Request 0.7 TON withdrawal
+          </a>
+        )}
       </div>
     </div>
   );
--- src/hooks/useMainContract.ts
+++ src/hooks/useMainContract.ts
@@ -2,10 +2,15 @@ import { useEffect, useState } from "react";
 import { MainContract } from "../contracts/MainContract";
 import { useTonClient } from "./useTonClient";
 import { useAsyncInitialize } from "./useAsyncInitialize";
-import { Address, OpenedContract } from "ton-core";
+import { Address, toNano, fromNano, OpenedContract } from "ton-core";
+import { useTonConnect } from "./useTonConnect";
 
 export function useMainContract() {
   const client = useTonClient();
+  const { sender } = useTonConnect();
+
+  const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
+
   const [contractData, setContractData] = useState<null | {
     counter_value: number;
     recent_sender: Address;
@@ -17,7 +22,7 @@ export function useMainContract() {
   const mainContract = useAsyncInitialize(async () => {
     if (!client) return;
     const contract = new MainContract(
-      Address.parse("EQDPo-Y2KBayhN86PntBicHrLEVOYNshUEzFEHkC7NPw2qmF") // replace with your address from tutorial 2 step 8
+      Address.parse("EQDPo-Y2KBayhN86PntBicHrLEVOYNshUEzFEHkC7NPw2qmF")
     );
     return client.open(contract) as OpenedContract<MainContract>;
   }, [client]);
@@ -34,13 +39,24 @@ export function useMainContract() {
         owner_address: val.owner_address,
       });
       setBalance(balance.number);
+      await sleep(5000); // sleep 5 seconds and poll value again
+      getValue();
     }
     getValue();
   }, [mainContract]);
 
   return {
     contract_address: mainContract?.address.toString(),
-    contract_balance: balance,
+    contract_balance: balance == null ? 0 : fromNano(balance),
     ...contractData,
+    sendIncrement: () => {
+        return mainContract?.sendIncrement(sender, toNano(0.05), 7);
+    },
+    sendDeposit: () => {
+      return mainContract?.sendDeposit(sender, toNano(1));
+    },
+    sendWithdrawalRequest: () => {
+      return mainContract?.sendWithdrawalRequest(sender, toNano(0.05), toNano(0.7));
+    },
   };
 }
